39d8393b8e237dca7a299947082af949a4a9c2ba
cj <920259081@qq.com>
2024-03-05 21:21:26 +0800

    3.5

--- prompt_work.py
+++ prompt_work.py
@@ -9,68 +9,117 @@ import os
 os.environ["http_proxy"] = "http://127.0.0.1:7890"
 os.environ["https_proxy"] = "http://127.0.0.1:7890"
 
+def century_prompt(dataset):
+    classes = [
+        "18",
+        "19",
+        "20"
+    ]
+    plm, tokenizer, model_config, WrapperClass = load_plm("bert", "bert-base-cased")
+    promptVerbalizer = ManualVerbalizer(
+        classes = classes,
+        label_words = {
+            "18": ["18th century", "1800s"],
+            "19": ["19th century", "1900s"],
+            "20": ["20th century", "2000s"]
+        },
+        tokenizer = tokenizer,
+    )
+    plm, tokenizer, model_config, WrapperClass = load_plm("bert", "bert-base-cased")
+    promptTemplate = ManualTemplate(
+        text = '{"placeholder":"text_a"} , As we can see from the above , {"placeholder":"text_b"} in the {"mask"}',
+        tokenizer = tokenizer,
+    )
+
+    promptModel = PromptForClassification(
+        template = promptTemplate,
+        plm = plm,
+        verbalizer = promptVerbalizer,
+    )
+
+    data_loader = PromptDataLoader(
+        dataset = dataset,
+        tokenizer = tokenizer,
+        template = promptTemplate,
+        tokenizer_wrapper_class=WrapperClass,
+    )
 
 
-classes = [ # There are two classes in Sentiment Analysis, one for negative and one for positive
-    "2009",
-    "2000"
-]
+    # making zero-shot inference using pretrained MLM with prompt
+    promptModel.eval()
+    with torch.no_grad():
+        for batch in data_loader:
+            logits = promptModel(batch)
+            preds = torch.argmax(logits, dim = -1)
+            print("century" + str(classes[preds]))
+
+def age_prompt(dataset):
+    classes = [ 
+        "0","1","2","3","4","5","6","7","8","9"
+    ]
+    plm, tokenizer, model_config, WrapperClass = load_plm("bert", "bert-base-cased")
+    promptVerbalizer = ManualVerbalizer(
+        classes = classes,
+        label_words = {
+            "0": ["00s", "aughts"],
+            "1": ["10s", "tens"],
+            "2": ["20s", "twenties"],
+            "3": ["30s", "thirties"],
+            "4": ["40s", "forties"],
+            "5": ["50s", "fifties"],
+            "6": ["60s", "sixties"],
+            "7": ["70s", "seventies"],
+            "8": ["80s", "eighties"],
+            "9": ["90s", "nineties"],
+        },
+        tokenizer = tokenizer,
+    )
+    plm, tokenizer, model_config, WrapperClass = load_plm("bert", "bert-base-cased")
+    promptTemplate = ManualTemplate(
+        text = '{"placeholder":"text_a"} , As we can see from the above , {"placeholder":"text_b"} in the {"mask"}',
+        tokenizer = tokenizer,
+    )
+
+    promptModel = PromptForClassification(
+        template = promptTemplate,
+        plm = plm,
+        verbalizer = promptVerbalizer,
+    )
+
+    data_loader = PromptDataLoader(
+        dataset = dataset,
+        tokenizer = tokenizer,
+        template = promptTemplate,
+        tokenizer_wrapper_class=WrapperClass,
+    )
+
+
+    # making zero-shot inference using pretrained MLM with prompt
+    promptModel.eval()
+    with torch.no_grad():
+        for batch in data_loader:
+            logits = promptModel(batch)
+            preds = torch.argmax(logits, dim = -1)
+            print("age" + str(classes[preds]))
+    # predictions would be 1, 0 for classes 'positive', 'negative'
+
 
 dataset = [ # For simplicity, there's only two examples
     # text_a is the input text of the data, some other datasets may have multiple input sentences in one example.
     InputExample(
         guid = 0,
         text_a = "Obama served as President of the United States at 2009",
+        text_b = "Obama served as President of the United States"
     ),
     InputExample(
         guid = 0,
-        text_a = "Obama served as President of the United States at 2000",
+        text_a = "Obama served as President of the United States at 1980",
+        text_b = "Obama served as President of the United States"
     )
 ]
 
-entity_subject = "Obama"
-entity_object = "President of the United States"
-predicate = "server"
-
-
-prompt_text = '{"placeholder":"text_a"} , '+ entity_subject + ' ' + predicate + ' ' + entity_object + ' at {"mask"}'
-plm, tokenizer, model_config, WrapperClass = load_plm("bert", "bert-base-cased")
-promptTemplate = ManualTemplate(
-    text = prompt_text,
-    tokenizer = tokenizer,
-)
-
-promptVerbalizer = ManualVerbalizer(
-    classes = classes,
-    label_words = {
-        "2009": ["2009"],
-        "2000": ["2000"],
-    },
-    tokenizer = tokenizer,
-)
-
-promptModel = PromptForClassification(
-    template = promptTemplate,
-    plm = plm,
-    verbalizer = promptVerbalizer,
-)
-
-data_loader = PromptDataLoader(
-    dataset = dataset,
-    tokenizer = tokenizer,
-    template = promptTemplate,
-    tokenizer_wrapper_class=WrapperClass,
-)
-
-
-# making zero-shot inference using pretrained MLM with prompt
-promptModel.eval()
-with torch.no_grad():
-    for batch in data_loader:
-        logits = promptModel(batch)
-        preds = torch.argmax(logits, dim = -1)
-        print(classes[preds])
-# predictions would be 1, 0 for classes 'positive', 'negative'
+century_prompt(dataset)
+age_prompt(dataset)
 
 
 
